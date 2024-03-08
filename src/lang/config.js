81df9a80dc319671ccda2f1b239f1292bb70f6d0
unknown <amethystedev@gmail.com>
2024-03-08 21:53:56 +0100

    create fr file, change config.js with fr lang reference

--- src/lang/config.js
+++ src/lang/config.js
@@ -1,8 +1,10 @@
 import en_GB from "@/lang/languages/en_GB";
+import fr_FR from "@/lang/languages/fr_FR";
 import it_IT from "@/lang/languages/it_IT";
 
 
 export const messages = {
     en_GB: en_GB,
+    fr_FR: fr_FR,
     it_IT: it_IT // Italian (Italy) | Italiano (Italia)
 }
